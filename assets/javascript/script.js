ebb764da9f41e98b59c69b968727983f53bd0735
Liz Emerson <uknity@gmail.com>
2021-03-23 10:13:48 -0400

    add buttons to the answer choices and an evaluate function to evaluate the answers

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,11 @@
 var mainSec = document.querySelector("main");
 
-var instructionsSec = document.querySelector("#instructions");
-var questionsSec = document.getElementById("questions-screen");
+var instructionsSec = document.querySelector(".instructions");
+
+var questionsSec = document.querySelector("#questions-section");
+
+var quesEl = document.querySelector("#ques");
+var ansEl = document.querySelector("#ans");
 
 var startBtn = document.querySelector("#start");
 
@@ -9,90 +13,139 @@ var quizEl = document.querySelector("#quiz");
 
 var pagesEl = document.querySelector(".pages");
 
-var messageCorrect = document.querySelector(".correct");
+var message = document.querySelector("message");
 
-var messageIncorrect = document.querySelector(".wrong");
+var finalScorePageEl = document.querySelector(".finalScorePage");
 
-var finalScorePageEl = document.querySelector("#finalScorePage");
+var finalScoreOutputEl = document.querySelector(".finalScoreOutput");
 
-var finalScoreOutputEl = document.querySelector("#finalScoreOutput");
+var initialsInput = document.querySelector(".initials");
 
-var initialsInput = document.querySelector("#initials");
+var highScoresPage = document.querySelector(".highScores");
 
-var highScoresPage = document.querySelector("#highScores");
+var highScoresInitialsEl = document.querySelector(".highScoresInitials");
 
-
-
-var questions = [
-    {
-        question: "Commonly used data types DO NOT include:",
-        answerChoices: {
-            1: "1. strings",
-            2: "2. booleans",
-            3: "3. alerts",
-            4: "4. numbers"
-        },
-
-        correctAnswer: 2,
-    },
-    {
-        question: "The condition in an if / else statement is enclosed within _____.",
-        answerChoices: {
-            1: "1. quotes",
-            2: "2. curly brackets",
-            3: "3. parenthesis",
-            4: "4. square brackets"
-        },
-        correctAnswer: 3,
-    },
-    {
-        question: "Arrays in JavaScript can be used to store ____.",
-        answerChoices: {
-            1: "1. numbers and strings",
-            2: "2. other arrays",
-            3: "3. booleans",
-            4: "4. all of the above"
-        },
-        correctAnswer: 4,
-    },
-    {
-        question: "String values must be enclosed within ____ when being assigned to variables.",
-        answerChoices: {
-            1: "1. commas",
-            2: "2. curly brackets",
-            3: "3. quotes", 
-            4: "4. parenthesis"
-        },
-        correctAnswer: 3,
-    },
-    {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answerChoices: {
-            1: "1. JavaScript",
-            2: "2. terminal / bash",
-            3: "3. for loops",
-            4: "4. console log",
-        },
-        correctAnswer: 4,
-    }
-]
-
-
+var currentQuestionIndex = 0;
 
 var score = 0;
 
-startBtn.addEventListener("click", startQuiz);
+// function nextQuestion (currentQuestionIndex) {
+//     for (i=0); i < 5; i++) {
+//         var displayQuestion = document.createElement("questions[i]");
+//         displayQuestion[0]
+//     }
+// }
 
+var questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    answerChoices: ["strings", "booleans", "alerts", "numbers"],
+    correctAnswer: "alerts",
+  },
+  {
+    question:
+      "The condition in an if / else statement is enclosed within _____.",
+    answerChoices: {
+      1: "1. quotes",
+      2: "2. curly brackets",
+      3: "3. parenthesis",
+      4: "4. square brackets",
+    },
+    correctAnswer: 3,
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ____.",
+    answerChoices: {
+      1: "1. numbers and strings",
+      2: "2. other arrays",
+      3: "3. booleans",
+      4: "4. all of the above",
+    },
+    correctAnswer: 4,
+  },
+  {
+    question:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    answerChoices: {
+      1: "1. commas",
+      2: "2. curly brackets",
+      3: "3. quotes",
+      4: "4. parenthesis",
+    },
+    correctAnswer: 3,
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answerChoices: {
+      1: "1. JavaScript",
+      2: "2. terminal / bash",
+      3: "3. for loops",
+      4: "4. console log",
+    },
+    correctAnswer: 4,
+  },
+];
 
-function startQuiz(){
-    instructionsSec
+// function showNextQuestion() {
+
+//     //clear out the last question
+//     //load the next question
+// }
+
+// questionDiv.value = question1
+// var one = 0;
+// var question1 = questions[one];
+
+function evaluate(event) {
+  console.log(event.target.value);
+  if (event.target.value == questions[0].correctAnswer) {
+    document.getElementById("message").innerHTML = "Correct!";
+  } else {
+    document.getElementById("message").innerHTML = "Wrong!";
+    //take away time
+  }
 }
-// console.log(questions);
+// function answers(questions[]) {
+//     for (var i = 0; i < questions[i].answerChoices.length; i++) {
+//         var answerButton = document.createElement("button");
+//         answerButton.textContent = i + 1 + ". " + questions[0].answerChoices[i];
+//         answerButton.setAttribute("value", questions[0].answerChoices[i]);
+//         answerElement.append(answerButton);
+//         answerButton.onclick = evaluate;
+//       }
+// }
 
-// console.log(questions[0]);
+var answerElement = document.getElementById("ans");
 
-// console.log(questions[0].answerChoices);
+function displayQuestion() {
+  for (i = 0; i < questions.length; i++) {
+    document.getElementById("ques").innerHTML = questions[i].question;
+    function answers() {
+        for (var i = 0; i < questions[i].answerChoices.length; i++) {
+            var answerButton = document.createElement("button");
+            answerButton.textContent = i + 1 + ". " + questions[0].answerChoices[i];
+            answerButton.setAttribute("value", questions[0].answerChoices[i]);
+            answerElement.append(answerButton);
+            answerButton.onclick = evaluate;
+          }
+    };
+    
+  }
 
-// console.log(questions[0].answerChoices[1]);
+  //   document.getElementById("ans").innerHTML =
+  //     questions[0].answerChoices.choice1 +
+  //     questions[0].answerChoices.choice2 +
+  //     questions[0].answerChoices.choice3 +
+  //     questions[0].answerChoices.choice4;
+  // ques1ans1.addEventListener("click", evaluate() {alert("click is working");});
+}
 
+function startQuiz() {
+  alert("start button activated");
+  instructionsSec.classList.add("hidden");
+  questionsSec.classList.remove("hidden");
+  displayQuestion();
+}
 
+startBtn.addEventListener("click", startQuiz);
