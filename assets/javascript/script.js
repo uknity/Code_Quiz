29ca3a78697c3ba8fd116224b5f8af0827822acc
Liz Emerson <uknity@gmail.com>
2021-03-24 08:18:12 -0400

    debugging finalScorePage to show up - still doesn't work

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,20 +14,13 @@ var quizEl = document.querySelector("#quiz");
 var pagesEl = document.querySelector(".pages");
 
 var messageEl = document.querySelector("#message");
-
-var finalScorePageEl = document.querySelector(".finalScorePage");
-
+var finalScorePageEl = document.getElementById("#finalScorePage");
 var finalScoreOutputEl = document.querySelector(".finalScoreOutput");
-
 var initialsInput = document.querySelector(".initials");
-
-var highScoresPage = document.querySelector(".highScores");
-
+var highScoresPage = document.getElementById("#highScores");
 var highScoresInitialsEl = document.querySelector(".highScoresInitials");
-
-var currentQuestionIndex = 0;
-
-var score = 0;
+var answerElement = document.getElementById("ans");
+var questionElement = document.getElementById("ques");
 
 var questions = [
   {
@@ -75,8 +68,21 @@ var questions = [
   },
 ];
 
-var answerElement = document.getElementById("ans");
-var questionElement = document.getElementById("ques");
+// function startTimer() {
+//   var counter = 75;
+
+//   var intervalId = setInterval(function () {
+//     console.log(counter);
+//     counter--;
+//     if (counter === 0) {
+//       alert("Time is up!");
+//       clearInterval(counter);
+//       quesEl.classList.add("hidden");
+//       finalScorePageEl.remove("hidden");
+//     }
+//   }, 1000);
+// }
+
 var currentQuestionIndex = 0;
 
 function evaluate(event) {
@@ -87,21 +93,27 @@ function evaluate(event) {
     messageEl.textContent = "Wrong!";
     //take away time
   }
+
   currentQuestionIndex++;
   var advanceButton = document.createElement("button");
   advanceButton.textContent = "Click to advance to the next question";
   messageEl.append(advanceButton);
+  
+  if (currentQuestionIndex > 4) {
+    quesEl.classList.add("hidden");
+    ansEl.classList.add("hidden");
+    finalScorePageEl.classlist.remove("hidden");
+    // finalScoreOutputEl.textContent = counter;
+  }
+  
   advanceButton.addEventListener("click", displayQuestion);
 }
 
 function answers() {
   var answers = questions[currentQuestionIndex].answerChoices;
   console.log(answers);
-  for (
-    var i = 0;
-    i < questions[currentQuestionIndex].answerChoices.length;
-    i++
-  ) {
+  for (var i = 0; i < questions[currentQuestionIndex].answerChoices.length; i++) 
+  {
     var answerButton = document.createElement("button");
     answerButton.textContent =
       i + 1 + ". " + questions[currentQuestionIndex].answerChoices[i];
@@ -111,20 +123,25 @@ function answers() {
     );
     answerElement.append(answerButton);
     answerButton.onclick = evaluate;
-  }
+  } 
 }
 
+
 function displayQuestion() {
-  if (currentQuestionIndex > 0) {
+  console.log(currentQuestionIndex);
+
+  
     answerElement.innerHTML = "";
     messageEl.textContent = "";
-  }
-  questionElement.innerHTML = questions[currentQuestionIndex].question;
-  answers();
+    questionElement.innerHTML = "";
+    questionElement.innerHTML = questions[currentQuestionIndex].question;
+    answers();
+  
 }
 
 function startQuiz() {
   alert("start button activated");
+  // startTimer();
   instructionsSec.classList.add("hidden");
   questionsSec.classList.remove("hidden");
   displayQuestion();
